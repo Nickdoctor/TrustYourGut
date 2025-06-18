beecc25f034e138025a3cb08dc3d9f8f93b1073a
Nickdoctor <ngugliemo@csus.edu>
2025-06-17 18:13:39 -0700

    Added app icon and slash screen

--- TrustYourGut/app/(tabs)/profile.tsx
+++ TrustYourGut/app/(tabs)/profile.tsx
@@ -2,19 +2,42 @@ import { Text, View, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from 'react';
 
 const PlaceholderImage = require('../../assets/images/profile.jpg'); 
 
 export default function ProfileScreen() {
+
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+ const resetPhoto = () => {
+  setSelectedImage(undefined);
+};
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Profile Screen</Text>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage}/>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
       </View>
       <View style = {styles.footerContainer}>
-        <Button theme="primary" label= "Choose a photo" />
-        <Button label = "Use this photo" />
+        <Button theme="primary" label= "Choose a photo" onPress={pickImageAsync} />
+        <Button label = "Reset Photo" onPress={resetPhoto} />
       </View>
     </View>
   );
