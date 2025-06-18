4a87d3c1414327d07271e720e516fd447cd679d9
Nickdoctor <ngugliemo@csus.edu>
2025-06-17 17:05:27 -0700

    Added default image for app, next is to allow user to select image

--- TrustYourGut/app/(tabs)/profile.tsx
+++ TrustYourGut/app/(tabs)/profile.tsx
@@ -1,10 +1,21 @@
 import { Text, View, StyleSheet } from 'react-native';
- import { Link } from 'expo-router'; 
+import { Link } from 'expo-router';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+
+const PlaceholderImage = require('../../assets/images/profile.jpg'); 
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Profile Screen</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage}/>
+      </View>
+      <View style = {styles.footerContainer}>
+        <Button theme="primary" label= "Choose a photo" />
+        <Button label = "Use this photo" />
+      </View>
     </View>
   );
 }
@@ -23,5 +34,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
